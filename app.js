60a579d0ef43c56ae4e42d270090edd561138ba3
omh02033 <omh02033@gmail.com>
2021-03-30 22:29:04 +0900

    set database password

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require("dotenv").config({
+    path: path.resolve(__dirname, "config/.env")
+});
 const express = require("express");
 const logger = require("morgan");
 const cookieParser = require('cookie-parser');
